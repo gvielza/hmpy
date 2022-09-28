f5c5badc7fa1e94b45763ac11a1a4b9ba607a738
gvielza <80008338+gvielza@users.noreply.github.com>
2022-09-28 13:04:56 -0300

    ej3

--- hola.py
+++ hola.py
@@ -1,4 +1,13 @@
+from unittest import result
+
+
 cadena="Hola Mundo!"
 print(cadena)
 cadena="Hola otra vez!"
 print (cadena)
+
+#ej3
+peso =int(input("¿Que peso en Kg usted tiene?"))
+altura=float(input("¿Que altura usted tiene en metros?"))
+result=peso//(altura**2)
+print("Su indice de masa corporal es :"+ str(result))
