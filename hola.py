f63a467ec50fbffd4496d78159739ddb34f97d9a
gvielza <80008338+gvielza@users.noreply.github.com>
2022-09-29 20:49:48 -0300

    ej5

--- hola.py
+++ hola.py
@@ -8,3 +8,19 @@ peso =int(input("¿Que peso en Kg usted tiene? "))
 altura=float(input("¿Que altura usted tiene en metros? "))
 result=peso//(altura**2)
 print("Su indice de masa corporal es :"+ str(result))
+
+#ej4
+lista=range(1,101)
+for numero in reversed(lista):
+    print (numero)
+
+#ej5
+def aBisiesto(numero):
+    if(numero%4==0):
+        print("El numero es bisiesto")
+    else:
+        print("no es un numero bisiesto")
+
+
+entrada=int(input("entre un número"))
+aBisiesto(entrada)
